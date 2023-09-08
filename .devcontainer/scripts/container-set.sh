938319c43605f8430cda24cc569ee6d699194502
glm2bg <121444468+glm2bg@users.noreply.github.com>
2023-09-08 13:03:23 +0200

    updated

--- .devcontainer/scripts/container-set.sh
+++
@@ -1,5 +0,0 @@
-echo "#######################################################"
-echo "### Checking container creation                     ###"
-echo "#######################################################"
-useradd vscode --password vscode -m
-usermod -aG sudo vscode
--- .devcontainer/scripts/postCreateCommand.sh
+++ .devcontainer/scripts/postCreateCommand.sh
@@ -1,8 +1,33 @@
+#!/bin/bash
+# Copyright (c) 2023 Robert Bosch GmbH
+#
+# This program and the accompanying materials are made available under the
+# terms of the Apache License, Version 2.0 which is available at
+# https://www.apache.org/licenses/LICENSE-2.0.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+set -e
+CLI_INSTALL_VERSION='latest' # reference a particular released version
 echo "#######################################################"
-echo "### Installing Velocitas CLI                        ###"
+echo "### Install Velocitas CLI ${CLI_INSTALL_VERSION}    ###"
 echo "#######################################################"
 
-apt update
-apt upgrade
-apt install curl
-curl https://github.com/eclipse-velocitas/cli/releases@latest
+CLI_ASSET_NAME=velocitas-linux-x64
+CLI_INSTALL_PATH=/usr/bin/velocitas
+
+CLI_DOWNLOAD_URL="https://github.com/eclipse-velocitas/cli/releases/${CLI_INSTALL_VERSION}/${CLI_ASSET_NAME}"
+
+echo "Downloading Velocitas CLI from ${CLI_DOWNLOAD_URL}"
+
+curl -L ${CLI_DOWNLOAD_URL} -o "${CLI_INSTALL_PATH}"
+chmod +x "${CLI_INSTALL_PATH}"
+
+CLI_VERSION=$( "${CLI_INSTALL_PATH}" --version )
+
+echo "Installed Velocitas CLI version: ${CLI_VERSION}"
