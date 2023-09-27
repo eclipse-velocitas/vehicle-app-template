4b99f3d809ca64bc6d1c5c11421fec00cc7be178
glm2bg <121444468+glm2bg@users.noreply.github.com>
2023-09-27 10:11:33 +0200

    test

--- .devcontainer/scripts/postCreateCommand.sh
+++ .devcontainer/scripts/postCreateCommand.sh
@@ -1,36 +1,3 @@
 #!/bin/bash
-# Copyright (c) 2023 Robert Bosch GmbH
-#
-# This program and the accompanying materials are made available under the
-# terms of the Apache License, Version 2.0 which is available at
-# https://www.apache.org/licenses/LICENSE-2.0.
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations
-# under the License.
-#
-# SPDX-License-Identifier: Apache-2.0
-set -e
-CLI_INSTALL_VERSION='v0.5.5' # reference a particular released version
-echo "#######################################################"
-echo "### Install Velocitas CLI ${CLI_INSTALL_VERSION}    ###"
-echo "#######################################################"
-
-ARH=$(uname -m)
-ARH=${ARH/aarch64/arm64}
-ARH=${ARH/x86_64/x64}
-CLI_ASSET_NAME=velocitas-linux-${ARH}
-CLI_INSTALL_PATH=/usr/bin/velocitas
-
-CLI_DOWNLOAD_URL="https://github.com/eclipse-velocitas/cli/releases/download/${CLI_INSTALL_VERSION}/${CLI_ASSET_NAME}"
-
-echo "Downloading Velocitas CLI from ${CLI_DOWNLOAD_URL}"
-
-sudo curl -L ${CLI_DOWNLOAD_URL} -o "${CLI_INSTALL_PATH}"
-sudo chmod +x "${CLI_INSTALL_PATH}"
-
-CLI_VERSION=$(sudo "${CLI_INSTALL_PATH}" --version )
-
-echo "Installed Velocitas CLI version: ${CLI_VERSION}"
+sudo chmod +x /home/vscode/configure-proxies.sh
+sudo ./home/vscode/configure-proxies.sh
