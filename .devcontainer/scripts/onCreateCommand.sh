cc7f43942a07de1dfeaf4cedc6d6ca1394be7896
Markus Petke <MP91@users.noreply.github.com>
2023-10-11 10:05:45 +0200

    rename postCreate script and call velocitas create postStart

---
+++ .devcontainer/scripts/onCreateCommand.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo chmod +x /home/vscode/configure-proxies.sh
+sudo /home/vscode/configure-proxies.sh > logs/configure-proxies.log 2>&1
--- .devcontainer/scripts/postCreateCommand.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo chmod +x /home/vscode/configure-proxies.sh
-sudo bash /home/vscode/configure-proxies.sh
